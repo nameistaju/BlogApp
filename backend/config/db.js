54147bf8ea90019b8832a9ecc831dfb228b75c05
Taj <bantu9848@gmail.com>
2025-05-15 15:31:53 +0530

    fix: use correct env var for Mongo URI

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,11 +3,20 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    const mongoURI = process.env.MONGO_URL;
+
+    if (!mongoURI) {
+      throw new Error("MONGO_URL is not defined in environment variables");
+    }
+
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
     console.log('MongoDB Connected...');
   } catch (err) {
     console.error(err.message);
-    // Exit process with failure
     process.exit(1);
   }
 };
